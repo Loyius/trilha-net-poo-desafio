d23bfbf787b5d980498dad5cac1055b9635792c2
Larissa Queiroz <larissaqramos1@gmail.com>
2024-11-10 17:27:15 -0300

    projeto finalizado

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -5,9 +5,9 @@ namespace DesafioPOO.Models
         public string Numero { get; set; }
 
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
-        public string Modelo { get; set; }
-        public string IMEI { get; set; }
-        public int Memoria { get; set; }
+        private string Modelo { get; set; }
+        private string IMEI { get; set; }
+        private int Memoria { get; set; }
 
         // TODO: Passar os parâmetros do construtor para as propriedades
         public Smartphone(string numero, string modelo, string imei, int memoria)
